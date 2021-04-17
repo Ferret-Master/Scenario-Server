669218bcfda8bb8ccfd217cba41c6294347332d6
Ferret-Master <64487611+Ferret-Master@users.noreply.github.com>
2021-04-17 09:13:47 +0800

    Huge amount of bug additions

--- ui/mods/scenario.server/icon_atlas.js
+++ ui/mods/scenario.server/icon_atlas.js
@@ -1,6 +1,11 @@
   
 var paeiouIcons = [
-    "blank_blip"
+    "blank_blip",
+	"big_grunt",
+	"grunt",
+	"spitter",
+	"warrior"
+	
 
 ];
 model.strategicIcons(model.strategicIcons().concat(paeiouIcons));
